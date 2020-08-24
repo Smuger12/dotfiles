badd310b09e900739288481e79e5eaff31dcbaf8
Smuger12 <eryk5188@gmail.com>
2020-08-24 20:10:08 +0200

    Added my scripts

---
+++ scripts/copy_configs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git_config=~/git/dotfiles/.config/
+git_home=~/git/dotfiles
+
+cp -r ~/.config/picom $git_config
+cp -r ~/.config/polybar $git_config
+cp -r ~/.config/kitty $git_config
+cp -r ~/.config/bspwm $git_config
+cp -r ~/.config/sxhkd $git_config
+cp -r ~/.config/alacritty $git_config
+cp -r ~/.config/neofetch $git_config
+cp -r ~/.zshrc $git_home
+cp -r ~/scripts $git_home
