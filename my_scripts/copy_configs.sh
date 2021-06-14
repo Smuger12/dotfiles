efa9ced61a2ad97f2da088a20757677dcf461117
Smuger12 <eryk5188@gmail.com>
2021-06-14 20:02:26 +0200

    Switched to sway

--- my_scripts/copy_configs.sh
+++ my_scripts/copy_configs.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-git_config=~/git/dotfiles/.config/
-git_home=~/git/dotfiles
+git_config=~/git-repo/dotfiles/.config
+git_home=~/git-repo/dotfiles
 
-cp -r ~/.config/picom $git_config
-cp -r ~/.config/polybar $git_config
-cp -r ~/.config/kitty $git_config
-cp -r ~/.config/bspwm $git_config
-cp -r ~/.config/sxhkd $git_config
-cp -r ~/.config/alacritty $git_config
-cp -r ~/.config/neofetch $git_config
-cp -r ~/.zshrc $git_home
-cp -r ~/my_scripts $git_home
+# cp -rv ~/.config/picom $git_config
+# cp -rv ~/.config/polybar $git_config
+# cp -rv ~/.config/kitty $git_config
+# cp -rv ~/.config/bspwm $git_config
+# cp -rv ~/.config/sxhkd $git_config
+cp -rv ~/.config/alacritty $git_config
+# cp -rv ~/.config/neofetch $git_config
+# cp -rv ~/.zshrc $git_home
+# cp -rv ~/my_scripts $git_home
+cp -rv ~/.config/sway/config $git_config/sway/
+cp -rv ~/.config/waybar/* $git_config/waybar/
+cp -rv ~/.config/fish/config.fish $git_config/fish/
+cp -rv ~/.config/starship.toml $git_config
