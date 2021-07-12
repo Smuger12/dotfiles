3f691bbcf74a764e1fefcc5bea6bb10556c83acd
Smuger12 <eryk5188@gmail.com>
2021-07-12 19:54:44 +0200

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,8 @@
+alias matrix="unimatrix -n -s 96 -l o"
+alias swayc="micro ~/.config/sway/config"
+alias waybarc="micro ~/.config/waybar/config"
+alias waybarcss="micro ~/.config/waybar/style.css"
+alias fishc="micro ~/.config/fish/config.fish"
+
+# launch starship prompt
 starship init fish | source
