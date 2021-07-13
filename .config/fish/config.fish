82185da0d66972a07d11031b234171fe8974e344
Smuger12 <eryk5188@gmail.com>
2021-07-13 13:58:01 +0200

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,12 @@ alias swayc="micro ~/.config/sway/config"
 alias waybarc="micro ~/.config/waybar/config"
 alias waybarcss="micro ~/.config/waybar/style.css"
 alias fishc="micro ~/.config/fish/config.fish"
-alias ipa='ip --color=always address'
-alias myip-'curl ipinfo.io/ip'
-alias off='systemctl poweroff'
-alias re='systemctl reboot'
-alias pacman='sudo pacman'
-alias c='clear'
+alias ipa="ip --color=always address"
+alias myip="curl ipinfo.io/ip"
+alias off="systemctl poweroff"
+alias re="systemctl reboot"
+alias pacman="sudo pacman"
+alias c="clear"
 
 # launch starship prompt
 starship init fish | source
