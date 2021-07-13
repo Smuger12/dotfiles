b6df01e4573b6cad63c9f36334eb51fd4be3ecb6
Eryk Pawlikowski <68848190+Smuger12@users.noreply.github.com>
2021-07-13 13:48:28 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,15 @@
+# my aliases
 alias matrix="unimatrix -n -s 96 -l o"
 alias swayc="micro ~/.config/sway/config"
 alias waybarc="micro ~/.config/waybar/config"
 alias waybarcss="micro ~/.config/waybar/style.css"
 alias fishc="micro ~/.config/fish/config.fish"
+alias ipa='ip --color=always address'
+alias myip-'curl ipinfo.io/ip'
+alias off='systemctl poweroff'
+alias re='systemctl reboot'
+alias pacman='sudo pacman'
+alias c='clear'
 
 # launch starship prompt
 starship init fish | source
